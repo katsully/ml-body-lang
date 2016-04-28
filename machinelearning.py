368092ce4dbd8793880483f54f5dce741cd84442
Kat Sullivan <kmsullivan012@gmail.com>
2016-04-28 14:47:59 -0400

    first commit

---
+++ machinelearning.py
@@ -0,0 +1,53 @@
+from sklearn.externals import joblib
+from pythonosc import dispatcher
+from pythonosc import osc_server
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+import argparse
+import time
+import random
+
+def print_click(*params):
+	# body_language = params[2:].split(",")
+	body_language = list(params[2:])
+	print (body_language)
+	if "N/A" in body_language:
+		print ("BAD DATA")
+		return
+	prediction  = clf.predict(body_language)
+	msg = osc_message_builder.OscMessageBuilder(address="/prediction")
+	pred_string = str(prediction[0])
+	print (pred_string)
+	msg.add_arg(pred_string)
+	msg = msg.build()
+	client.send(msg)
+
+if __name__ == "__main__":
+	clf = joblib.load('machinelearning.pkl')
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--ip", default="127.0.0.1",
+		help="the ip of the osc server")
+	parser.add_argument("--port", type=int, default=8000,
+		help="The port the OSC server is listening on")
+	args = parser.parse_args()
+
+	# needs to be 9000 for beginning loop
+	client = udp_client.UDPClient(args.ip, 9000)
+
+	dispatcher = dispatcher.Dispatcher()
+	dispatcher.map("/skeletal_data", print_click, "Click")
+
+	# needs to be port 9000 to receive data from cinder
+	server = osc_server.ThreadingOSCUDPServer(
+		(args.ip, 8000), dispatcher)
+	print("Serving on {}".format(server.server_address))
+	for x in range(5):
+		msg = osc_message_builder.OscMessageBuilder(address="/prediction")
+		msg.add_arg(random.random())
+		msg = msg.build()
+		client.send(msg)
+		time.sleep(1)
+	server.serve_forever()
+
+
